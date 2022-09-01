4a6ea7dfb39d0a99f304e8efc133f75ce92b7560
Mustapha Awwal <awwalmustapha41@gmail.com>
2022-09-01 12:56:41 +0100

    task 1 not finished

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" this is the base model that all common attributes/methods for other classes"""
+
+import uuid
+import datetime
+
+
+class BaseModel:
+
+    """ the base model"""
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
