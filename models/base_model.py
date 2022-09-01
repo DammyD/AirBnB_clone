f6454438dc1b17466cef36d9dac0bfa9c87c5434
DammyD <ibukunoluwadawodu@gmail.com>
2022-09-01 14:01:55 -0700

    base_model.py - updated

--- models/base_model.py
+++ models/base_model.py
@@ -11,11 +11,19 @@ import datetime
 class BaseModel:
     """ the base model"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """initialize object with the attributes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == __class__:
+                    continue
+                if key == 'created_at' or key == 'update_at':
+                    value = datetime.datetime.fromisoformat(value)
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """ the string for instance
