00c67aa92a3477128db20a7c99306c1ff22ff1a8
Mustapha Awwal <awwalmustapha41@gmail.com/>
2022-09-01 07:17:32 -0700

    correcting basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" 
-this is the base model that all common attributes/methods 
+"""
+this is the base model that all common attributes/methods
 for other classes
 """
 
@@ -15,4 +15,3 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
-        
