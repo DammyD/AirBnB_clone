8473e9d049329b8b8d114cde5a137d802fd55448
Mustapha Awwal <101365610+MustaphaAwwal@users.noreply.github.com>
2022-09-01 17:01:05 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -12,9 +12,7 @@ class BaseModel:
     """ the base model"""
 
     def __init__(self):
-	"""
-	initialize object with the attributes
-	"""
+        """initialize object with the attributes"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
@@ -22,7 +20,7 @@ class BaseModel:
     def __str__(self):
         """ the string for instance
 	class name, id, dict
-	"""
+        """
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
